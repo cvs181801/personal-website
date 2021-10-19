973e33985deaab4a0ba6260166ded1b9bc4243c4
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-18 18:46:24 -0700

    add some shapes

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 //Ways I use technology for good
 const essentialsDiv = document.querySelector(".essentials");
 
-const theEssentials = ["Using secure coding practices", "Creating meaningful plaforms", 
-"Lifelong learning", "Creating amazing experiences", "Making the web safer for all"];
 
-function renderTheEssentials() {
-    for(essential of theEssentials) {
-        essentialItem = document.createElement('p');
-        essentialItem.textContent = `${essential} is what I do`;
-        essentialsDiv.append(essentialItem);
-    }
-}
-
-renderTheEssentials();
 
 
