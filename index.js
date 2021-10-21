3710513f9fd4b3b4992fad57c65b9901027c7e0b
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-20 17:05:54 -0700

    save progress on the for loop

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-//Ways I use technology for good
-const essentialsDiv = document.querySelector(".essentials");
-
-console.log(window.screen.width);
+//grab elements from the DOM
+const cubes = document.querySelectorAll(".cube");
 
 
+for (cube=0; cube < cubes.length; cube++) {
+            cubes[cube].addEventListener("mouseover", function() {
+                //cubes[cube].style.transform = "translate3d(.8em, -0.8em, .8em)";
+                //cubes[cube].style.transition = "all 1s ease-in-out";
+                console.log("hovered")
+            })
+}
+
+// .project1:hover {
+//     transform: translate3d(.8em, -0.8em, .8em);
+//     transition: all 1s ease-in-out;
+// }
